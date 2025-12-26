5306ce1aa0974abac5b5915c8a03090a6760458f
Rahul kommavarapu <Rahulkommavarapu225@gmail.com>
2025-12-26 23:34:00 +0530

    Expense

--- Mysql.sh
+++ Mysql.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-USERID=$(id-U)
+USERID=$(id -u)
 
 R="\e[31m"
 G="\e[32m"
 Y="\e[0m"
  
- LOGS_FOLDER="var/log/Expense-shell.logs"
- LOGS_FILE=$(echo$0/cut-d"." -f1)
+ LOGS_FOLDER="/var/log/expense-logs"
+ LOGS_FILE=LOG_FILE=$(echo $0 | cut -d "." -f1)
  TIMESTAMP=$(date +%Y-%m-%H-%M-%S)
  LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
+
  VALIDATE(){
-       if [$1 -ne 0]
+         if [ $1 -ne 0 ]
        then
             echo -e "$2 ... $R FAILURE $N"
             exit 1
@@ -45,6 +46,7 @@ Y="\e[0m"
 
  VALIDATE $? "starting mysql server"
 
- mysql_secure_installation --set--root--pass ExpenseApp@1
+ mysql_secure_installation --set-root-pass ExpenseApp@1
+
 
  VALIDATE $? "Setting Root Password"
