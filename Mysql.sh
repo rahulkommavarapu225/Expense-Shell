d3ba2ead26021d773ffdaa1ce329b9e5cfdd31fb
Rahul kommavarapu <Rahulkommavarapu225@gmail.com>
2026-01-12 23:50:11 +0530

    Expense

--- Mysql.sh
+++ Mysql.sh
@@ -1,66 +1,50 @@
 #!/bin/bash
 
 USERID=$(id -u)
-
+#Colours
 R="\e[31m"
 G="\e[32m"
-Y="\e[0m"
- 
- LOGS_FOLDER="/var/log/expense-logs"
- LOGS_FILE=LOG_FILE=$(echo $0 | cut -d "." -f1)
- TIMESTAMP=$(date +%Y-%m-%H-%M-%S)
- LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+Y="\e[33m"
+N="\e[34m"
 
+LOGS_FOLDER= "/var/log/expense-logs"
+LOG_FILE=$(echo $0 |cut -d "." -f1)
+TIMESTAMP=$(date +%y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log" 
 
- VALIDATE(){
-         if [ $1 -ne 0 ]
-       then
-            echo -e "$2 ... $R FAILURE $N"
-            exit 1
-        else 
-           echo -e "$2 ... $G SUCCESS $N"
-        fi
- }
-
- CHECK_ROOT(){
-    if [ $USERID -ne 0 ]
+#VAlidate the Script of Mysqld
+VALIDATE (){
+   if [ $1 -ne 0 ] 
     then
-         echo "ERROR:: You must have sudo access to execute this script"
-        exit 1 #other than 0
-   fi
- }
-
- echo "Script stated executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
- 
- CHECK_ROOT
-
- dnf install mysql-server -y &>>$LOG_FILE_NAME
-
- VALIDATE $? "Installing mysql server"
-
- systemctl enable mysqld &>>$LOG_FILE_NAME
-
- VALIDATE $? "Enabling mysql server"
-
- systemctl start mysqld &>>$LOG_FILE_NAME
-
- VALIDATE $? "starting mysql server"
-  mysql -h mysqldata.practice25.online -u root -pExpenseApp@1 -e 'show databases;' &>>$LOG_FILE_NAME
-
-  if [$? -ne 0]
-  then 
-     echo "MySQL Root password not setup" &>>$LOG_FILE_NAME
-     mysql_secure_installation --set-root-pass ExpenseApp@1
-     VALIDATE $? "Setting Root Password"
+       echo -e "$2......$R FAILURE $N"
+       exit1
+     else
+      echo -e "$2......$R SUCCESS $N"
+    fi
+}
+# Check the Root access
+CHECK_ROOT (){   
+  if [ $USERID -ne 0 ]
+   then
+   echo "ERROR:: you must have Access to execute this script"
+   exit 1
    else
-    echo -e "MySQL Root password already setup ...$Y SKIPPING $N"
-   fi   
+    echo "Continue the script"
+  fi
+     }
 
+echo "Script started executing at:$TIMESTAMP" & >>$LOG_FILE_NAME
 
+dnf install mysql-server -y 
+VALIDATE $? "Installing mysql server"
 
+systemctl enable mysqld 
+VALIDATE $? "Enabling the Mysql-server"
 
+systemctl start mysqld 
+VALIDATE $? "Starting Mysql-server"
 
- mysql_secure_installation --set-root-pass ExpenseApp@1
-
-
- VALIDATE $? "Setting Root Password"
+# setting root-Password
+mysql_secure_installation  ---set--root---pass ExpenseAPP@1
+VALIDATE $? "setting root password"
+ 
