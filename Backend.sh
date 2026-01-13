1d84e91e9a729c22765dc8633b6d391ec36a79ee
Rahul kommavarapu <Rahulkommavarapu225@gmail.com>
2026-01-14 01:03:14 +0530

    Expense

--- Backend.sh
+++ Backend.sh
@@ -8,13 +8,13 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[34m"
 
-LOGS_FOLDER= "var/log/expense-logs"
+LOGS_FOLDER="var/log/expense-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
 
-VALIDATE(){
+VALIDATE() {
     if [ $1 -ne 0 ]
     then
      echo -e "$2......$R FAILURE $N"
@@ -24,8 +24,8 @@ VALIDATE(){
     fi 
 }
 
-CHECK_ROOT(){
-    if [ USERID -ne 0 ]
+CHECK_ROOT() {
+    if [ $USERID -ne 0 ]
     then
       echo "ERROR:: you must have sudo access to execute this Script"
       exit 1
@@ -37,19 +37,19 @@ echo "Script Started executing at :$TIMESTAMP "
 dnf module disable nodejs -y &>>$LOG_FILE_NAME
 VALIDATE $? "Disabling existing default Nodejs"
 
-dnf module Enable nodejs:20 -y &>>$LOG_FILE_NAME
-VALIDATE $? "Enableing Nodejs:20"
+dnf module enable nodejs:20 -y &>>$LOG_FILE_NAME
+VALIDATE $? "Enableing Nodejs:20 "
 
 dnf install nodejs -y &>>$LOG_FILE_NAME
 VALIDATE $? "Installig nodejs"
 
-Useradd Expense &>>$LOG_FILE_NAME
+useradd expense &>>$LOG_FILE_NAME
 VALIDATE $? "Adding/Creating Expense User"
 
 mkdir /app &>>$LOG_FILE_NAME
 VALIDATE $? "Creating app Directory"
 
-curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip & >>$LOG_FILE_NAME
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip  &>>$LOG_FILE_NAME
 VALIDATE $? "Downloading Backend"
 
 cd /app  #Change the App Directory
@@ -60,14 +60,15 @@ VALIDATE $? "Unzip Backend"
 npm install &>>$LOG_FILE_NAME
 VALIDATE $? "Installing Dependencies"
 
-cp /home/ec2-user/Expense-Shell/backend.service/etc/systemd/system/backend.service
+cp /home/ec2-user/Expense-Shell/backend.service /etc/systemd/system/backend.service
 #Prepare Mysql Schema
 
 dnf install mysql -y &>>$LOG_FILE_NAME
 VALIDATE $? "Installing Mysql Client"
 
-Mysql -h mysql.practice25.online -uroot -pExpenseApp@1 < /app/schema/backend.sqlVALIDATE $? "Setting up the Transactions schema and Tables"&>>$LOG_FILE_NAME
-VALIDATE $? "Setting up the Trasactions schema and Tables "
+ mysql -h mysql.practice25.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE_NAME
+VALIDATE $? "Setting up the Transactions schema and Tables"
+
 
 systemctl daemon-reload &>>$LOG_FILE_NAME
 VALIDATE $? "Daemon-reload"
