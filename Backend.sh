72eb4e63458e891db43689a60f9f36c651cfcb93
Rahul kommavarapu <Rahulkommavarapu225@gmail.com>
2026-01-14 13:15:02 +0530

    Extra2

--- Backend.sh
+++ Backend.sh
@@ -47,8 +47,7 @@ VALIDATE $? "Enableing Nodejs:20 "
 dnf install nodejs -y &>>$LOG_FILE_NAME
 VALIDATE $? "Installig nodejs"
 
-useradd expense &>>$LOG_FILE_NAME
-VALIDATE $? "Adding/Creating Expense User"
+
 
 mkdir /app &>>$LOG_FILE_NAME
 VALIDATE $? "Creating app Directory"
