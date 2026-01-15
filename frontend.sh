fe8beed1d53323f9e5e9c498245c038b6da5f667
Rahul kommavarapu <Rahulkommavarapu225@gmail.com>
2026-01-16 00:52:38 +0530

    execute

--- frontend.sh
+++ frontend.sh
@@ -60,7 +60,7 @@ VALIDATE $? "Moving to HTML Directory"
 unzip /tmp/frontend.zip  &>>$LOG_FILE_NAME
 VALIDATE $? "Unzipping the frontend code"
 
-cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf
+cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf &>>$LOG_FILE_NAME
 VALIDATE $? "Copied expense config"
 
 systemctl restart nginx  &>>$LOG_FILE_NAME
